9205fe6688ffd77ee35b71d6e426e1bd28ca5c76
Lyenx <105422453+ImLyenx@users.noreply.github.com>
2024-04-29 11:50:25 +0200

    turn back

--- Assets/SSL/Runtime/Scripts/Hero/HeroEntity.cs
+++ Assets/SSL/Runtime/Scripts/Hero/HeroEntity.cs
@@ -24,8 +24,15 @@ public class HeroEntity : MonoBehaviour
 
     private void FixedUpdate()
     {
-        _UpdateHorizontalSpeed();
-        _ChangeOrientFromHorizontalMovement();
+        if (_AreOrientAndMovementOpposite())
+        {
+            _TurnBack();
+        }
+        else
+        {
+            _UpdateHorizontalSpeed();
+            _ChangeOrientFromHorizontalMovement();
+        }
         _ApplyHorizontalSpeed();
     }
 
@@ -71,6 +78,21 @@ public class HeroEntity : MonoBehaviour
         {
             _horizontalSpeed = 0f;
         }
+    }
+
+    private void _TurnBack()
+    {
+        _horizontalSpeed -= _movementsSettings.turnBackFriction * Time.fixedDeltaTime;
+        if (_horizontalSpeed < 0f)
+        {
+            _horizontalSpeed = 0f;
+            _ChangeOrientFromHorizontalMovement();
+        }
+    }
+
+    private bool _AreOrientAndMovementOpposite()
+    {
+        return _moveDirX * _orientX < 0f;
     }
     
     private void Update()
--- Assets/SSL/Runtime/Scripts/Hero/HeroHorizontalMovementsSettings.cs
+++ Assets/SSL/Runtime/Scripts/Hero/HeroHorizontalMovementsSettings.cs
@@ -6,5 +6,6 @@ public class HeroHorizontalMovementsSettings
 {
     public float acceleration = 20f;
     public float deceleration = 15f;
+    public float turnBackFriction = 25f;
     public float maxSpeed = 5f;
 }
