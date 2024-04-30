8d017fe794de652aa2d741fbc43f0a5d25b47427
Lyenx <105422453+ImLyenx@users.noreply.github.com>
2024-04-30 12:07:05 +0200

    jump buffer

--- Assets/SSL/Runtime/Scripts/Hero/HeroController.cs
+++ Assets/SSL/Runtime/Scripts/Hero/HeroController.cs
@@ -6,6 +6,11 @@ public class HeroController : MonoBehaviour
     [SerializeField]
     private HeroEntity _entity;
 
+    [Header("Jump Buffer")]
+    [SerializeField]
+    private float _jumpBufferDuration = 0.2f;
+    private float _jumpBufferTimer = 0f;
+
     [Header("Debug")]
     [SerializeField]
     private bool _guiDebug = false;
@@ -17,14 +22,34 @@ public class HeroController : MonoBehaviour
 
         GUILayout.BeginVertical(GUI.skin.box);
         GUILayout.Label(gameObject.name);
+        GUILayout.Label($"Jump Buffer Timer = {_jumpBufferTimer}");
         GUILayout.EndVertical();
+    }
+
+    private void Start()
+    {
+        _CancelJumpBuffer();
     }
 
     private void Update()
     {
+        _UpdateJumpBuffer();
+
         _entity.SetMoveDirX(GetInputMoveX());
 
         if (_GetInputDownJump())
+        {
+            if (_entity.IsTouchingGround && !_entity.IsJumping)
+            {
+                _entity.JumpStart();
+            }
+            else
+            {
+                _ResetJumpBuffer();
+            }
+        }
+
+        if (IsJumpBufferActive())
         {
             if (_entity.IsTouchingGround && !_entity.IsJumping)
             {
@@ -63,5 +88,27 @@ public class HeroController : MonoBehaviour
     private bool _GetInputJump()
     {
         return Input.GetKey(KeyCode.Space);
+    }
+
+    private void _ResetJumpBuffer()
+    {
+        _jumpBufferTimer = 0f;
+    }
+
+    private bool IsJumpBufferActive()
+    {
+        return _jumpBufferTimer < _jumpBufferDuration;
+    }
+
+    private void _UpdateJumpBuffer()
+    {
+        if (!IsJumpBufferActive())
+            return;
+        _jumpBufferTimer += Time.deltaTime;
+    }
+
+    private void _CancelJumpBuffer()
+    {
+        _jumpBufferTimer = _jumpBufferDuration;
     }
 }
